a7ca6f45390e79ae707bc28dcfd44c3d01401d7a
Wtower <gkarak@9-dev.com>
2017-02-01 14:28:41 +0200

    Use YAML-based configuration

--- README.rst
+++ README.rst
@@ -1,8 +1,39 @@
 sysadmin-backup
 ===============
 
-Console backup application.
+Console backup application. Intended mainly for server administration.
 
-This is the Version 2 in Python. For Version 1.20 in C++ see `sysadmin-backup v1`_.
+Features
+--------
+
+- Configuration_ is YAML based
+- Full logging_ capabilities with multiple handlers_ available
+
+.. _Configuration: https://github.com/Wtower/sysadmin-backup2/blob/master/conf/sample.conf.yaml
+.. _logging: https://docs.python.org/3/library/logging.config.html#dictionary-schema-details
+.. _handlers: https://docs.python.org/3/library/logging.handlers.html#module-logging.handlers
+
+Usage
+-----
+::
+
+    (sysadmin-backup2) gkarak@neil ~/w/p/sysadmin-backup2 (dev)> ./sysadmin-backup.py -h
+    usage: sysadmin-backup.py [-h] [-v] [--version] conf_file
+
+    sysadmin-backup
+
+    positional arguments:
+      conf_file   Configuration file
+
+    optional arguments:
+      -h, --help  show this help message and exit
+      -v          Verbosity level
+      --version   show program's version number and exit
+
+
+Version note
+------------
+
+This is the Version 2 in Python 3. For Version 1.20 in C++ see `sysadmin-backup v1`_.
 
 .. _sysadmin-backup v1: https://github.com/Wtower/sysadmin-backup
--- core/app.py
+++ core/app.py
@@ -1,14 +1,28 @@
 """ Main application """
+import logging
+import logging.config
 import argparse
-from core.conf import Configuration
+from ruamel import yaml
 
 __version__ = 'v2.0.0'
 
 
 class Backup:
+    logging_handler_console_level_default = logging.CRITICAL
+
     def __init__(self):
-        parser = argparse.ArgumentParser(description="sysadmin-backup")
-        parser.add_argument('conf_file', type=open, action=Configuration, help="Configuration file")
-        parser.add_argument('--excludes', required=True, help="A list of paths to exclude")
-        args = parser.parse_args()
-        print(args)
+        parser = argparse.ArgumentParser(description='sysadmin-backup')
+        parser.add_argument('conf_file', type=open, help="Configuration file")
+        parser.add_argument('-v', action='count', default=0, dest='verbosity', help="Verbosity level")
+        parser.add_argument('--version', action='version', version=parser.description + ' ' + __version__)
+        arguments = parser.parse_args()
+
+        self.configuration = yaml.load(arguments.conf_file.read(), yaml.RoundTripLoader)
+        self.logging_handler_console_level_default -= arguments.verbosity * 10
+        self.configuration['logging']['handlers']['console']['level'] = self.logging_handler_console_level_default
+        logging.config.dictConfig(self.configuration['logging'])
+        logger = logging.getLogger('backup')
+
+        print(parser.description)
+        logger.debug(arguments)
+        logger.debug(self.configuration['backup'])
