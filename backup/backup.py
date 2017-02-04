25fef31aef2028705899671802cd2d89ccd02de7
Wtower <gkarak@9-dev.com>
2017-02-04 15:17:42 +0200

    WIP #5: mysql dump

--- backup/backup.py
+++ backup/backup.py
@@ -1,21 +1,43 @@
 """ Abstract class for all backup methods """
 import time
+import logging
+import os
+import sh
 from abc import ABCMeta, abstractmethod
 from core.mount import Mount
 
 
+# noinspection PyUnresolvedReferences
 class BackupMethod:
     __metaclass__ = ABCMeta
     lock_file = '/tmp/sysadmin-backup'
 
-    def __init__(self, configuration):
+    def __init__(self, configuration, arguments):
         self.start_time = time.time()
         self.configuration = configuration
+        self.arguments = arguments
         self.mount = Mount(configuration['destination'])
+        self.logger = logging.getLogger('backup')
 
     def stat(self):
         return time.time() - self.start_time
 
+    def mysql_dump(self):
+        if self.configuration['backup'].get('mysqldump', None):
+            destination = None
+            if not self.arguments.dry_run:
+                destination = self.configuration['destination'].get(
+                    'database',
+                    self.configuration['destination']['destination'])
+                destination = os.path.join(destination, 'mysqldump.sql')
+            self.logger.debug('Mysqldump destination: %s', destination)
+            try:
+                sh.mysqldump('--defaults-extra-file=~/.my.cnf', '--all-databases', _out=destination)
+            except sh.ErrorReturnCode as exc:
+                self.logger.error("Unable to perform mysqldump: %s", exc.stderr)
+            else:
+                self.logger.debug("Performed mysql dump")
+
     @abstractmethod
     def execute(self):
-        pass  # pragma: nocover
+        self.mysql_dump()
--- tests/tests.py
+++ tests/tests.py
@@ -8,18 +8,23 @@ from core.app import Backup
 
 
 class BackupTestCase(unittest.TestCase):
+    @patch('backup.backup.sh')
     @patch('core.mount.sh')
-    def test_backup_external(self, mock_sh):
+    def test_backup_external(self, mock_sh, mock_sh_backup):
         sys.argv = [sys.argv[0], '-vvvv', 'conf/sample_external.conf.yml']
         Backup()
         self.assertIn(call.mount('/dev/sdf', '/tmp/spufd2'), mock_sh.method_calls)
         self.assertIn(call.umount('-l', '/dev/sdf'), mock_sh.method_calls)
         stderr = sys.stderr.getvalue()
         self.assertIn('conf/sample_external.conf.yml', stderr)
-        self.assertIn('DEBUG Mounted device', stderr)
-        self.assertIn('DEBUG Unmounted device', stderr)
-        stdout = sys.stderr.getvalue()
+        self.assertIn("DEBUG Mounted device", stderr)
+        # self.assertIn("DEBUG Performed mysql dump", stderr)
+        self.assertIn("DEBUG Unmounted device", stderr)
+        self.assertIn("DEBUG Elapsed time", stderr)
+        stdout = sys.stdout.getvalue()
         self.assertIn('sysadmin-backup', stdout)
+        self.assertIn("Performing backup", stdout)
+        self.assertIn('Backup finished', stdout)
 
     class MockShMounted(Mock):
         # noinspection PyPep8Naming
@@ -37,8 +42,8 @@ class BackupTestCase(unittest.TestCase):
         Backup()
         stderr = sys.stderr.getvalue()
         self.assertIn('conf/sample_external.conf.yml', stderr)
-        self.assertIn('DEBUG Device already mounted', stderr)
-        self.assertIn('DEBUG Unmounted device', stderr)
+        self.assertIn("DEBUG Device already mounted", stderr)
+        self.assertIn("DEBUG Unmounted device", stderr)
 
     @patch('core.mount.sh')
     def test_backup_external_encrypted(self, mock_sh):
@@ -54,15 +59,15 @@ class BackupTestCase(unittest.TestCase):
         self.assertIn(call.cryptsetup('luksClose', 'backup-external'), mock_sh.method_calls)
         stderr = sys.stderr.getvalue()
         self.assertIn('conf/sample_external_enc.conf.yml', stderr)
-        self.assertIn('DEBUG Opened encrypted device', stderr)
-        self.assertIn('DEBUG Closed encrypted device', stderr)
+        self.assertIn("DEBUG Opened encrypted device", stderr)
+        self.assertIn("DEBUG Closed encrypted device", stderr)
 
     def test_backup_local(self):
         sys.argv = [sys.argv[0], '-vvvv', 'conf/sample_local.conf.yml']
         Backup()
         stderr = sys.stderr.getvalue()
         self.assertIn('conf/sample_local.conf.yml', stderr)
-        self.assertNotIn('DEBUG Mounted device', stderr)
+        self.assertNotIn("DEBUG Mounted device", stderr)
 
     def tearDown(self):
         shutil.rmtree('/tmp/spufd2')
