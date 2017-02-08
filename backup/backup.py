8d13dea7e1da111246bf613900da342cb3dcadb9
Wtower <gkarak@9-dev.com>
2017-02-08 03:54:07 +0200

    Add new frequency conf options and improved loggers

--- backup/backup.py
+++ backup/backup.py
@@ -11,13 +11,20 @@ from core.mount import Mount
 class BackupMethod:
     __metaclass__ = ABCMeta
     lock_file = '/tmp/sysadmin-backup'
+    frequencies = {
+        'daily': '%Y-%m-%d',
+        'weekly': '%Y-W%W',
+        'monthly': '%Y-%m',
+        'yearly': '%Y'
+    }
 
     def __init__(self, configuration, arguments):
         self.start_time = time.time()
         self.configuration = configuration
         self.arguments = arguments
         self.mount = Mount(configuration['destination'])
-        self.logger = logging.getLogger('backup')
+        # Default values
+        self.conf_file_name = os.path.basename(self.arguments.conf_file.name).split('.')[0]
         self.configuration['destination']['database'] = self.configuration['destination'].get(
             'database', self.configuration['destination']['destination'])
 
@@ -26,27 +33,29 @@ class BackupMethod:
 
     def mysql_dump(self):
         if self.configuration['backup'].get('mysqldump', None):
+            logger = logging.getLogger('backup.mysqldump')
             destination = os.path.join(self.configuration['destination']['database'], 'mysqldump.sql')
             if self.arguments.dry_run:
                 destination = None
             try:
                 sh.mysqldump('--defaults-extra-file=~/.my.cnf', '--all-databases', _out=destination)
             except sh.ErrorReturnCode as exc:
-                self.logger.error("Unable to perform mysqldump: %s", exc.stderr)
+                logger.error("Unable to perform mysqldump: %s", exc.stderr)
             else:
-                self.logger.debug("Performed mysql dump")
+                logger.debug("Performed mysql dump")
 
     def postgres_dump(self):
         if self.configuration['backup'].get('postgresdump', None):
+            logger = logging.getLogger('backup.postgresdump')
             destination = os.path.join(self.configuration['destination']['database'], 'postgresdump.sql')
             if self.arguments.dry_run:
                 destination = None
             try:
                 sh.pg_dumpall(_out=destination)
             except sh.ErrorReturnCode as exc:
-                self.logger.error("Unable to perform postgresdump: %s", exc.stderr)
+                logger.error("Unable to perform postgresdump: %s", exc.stderr)
             else:
-                self.logger.debug("Performed postgres dump")
+                logger.debug("Performed postgres dump")
 
     @abstractmethod
     def execute(self):
